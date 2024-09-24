966303187f8e8964afa0c92582b5371e598112a1
cliffootieno <clifford242526@gmail.com>
2024-09-24 05:21:36 +0300

    initial commit

--- main.go
+++ main.go
@@ -11,13 +11,15 @@ func main() {
 	input := bufio.NewScanner(os.Stdin)
 	// var out struct{}
 	tasks := []string{}
+	count:=0
 	for input.Scan() {
 		line := input.Text()
-	tasks = append(tasks, line)
-		if line==""{
+		tasks = append(tasks, line)
+		if count==2{
 			break
 		}
-}
-service.Input(tasks)
-	// fmt.Println(out)
+		count++
+	}
+	service.Input(tasks)
+	// fmt.Println(out)~
 }
--- service/input.go
+++ service/input.go
@@ -3,31 +3,41 @@ package service
 import (
 	"fmt"
 	"strconv"
+	"sync"
 	"time"
 )
 
 var Words struct {
-	name string
-	task string
-	time time.Time
+	name  string
+	task  string
+	time  time.Time
+	alarm time.Timer
 }
 
 func Input(str []string) {
-for i:=range str{
-	switch i {
-	case 0:
-		Words.name = str[i]
-	case 1:
-		Words.task = str[i]
-	case 2:
-		timed, _ := strconv.Atoi(str[i])
-		Words.time = time.Now().Add(time.Duration(timed) * time.Hour)
-	}
+	var wg sync.WaitGroup
+	for i := range str {
+		switch i {
+		case 0:
+			Words.name = str[i]
+		case 1:
+			Words.task = str[i]
+		case 2:
+			timed, _ := strconv.Atoi(str[i])
+			Words.time = time.Now().Add(time.Duration(timed) * time.Second)
+			Words.alarm = *time.NewTimer(time.Until(Words.time))
+		}
 
+		if i == 2{
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				<-Words.alarm.C
+				fmt.Printf("%s time\n",Words.task)
+			}()
 
-	if  i > 2 {
-		// return Words
-		fmt.Printf("Hi %s,your task %s is at %s\n",Words.name,Words.task, Words.time.Format("2006-01 15:04:05"))
+			fmt.Printf("Hi %s,your task \"%s\" is at %s\n", Words.name, Words.task, Words.time.Format("2006-01 15:04:05"))
+		}
 	}
-}
+	wg.Wait()
 }
