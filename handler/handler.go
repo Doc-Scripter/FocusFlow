d250d0a6074ec0185ad3fe0dc6147eb520f1c02b
LuvDokta <clifford242526@gmail.com>
2024-09-28 22:06:31 +0300

    "Added NewEvent handler, template, and route; updated main.go to handle new routes; modified index.html to link to new routes"

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,20 @@
 package handler
 
 import (
+	"encoding/json"
 	"html/template"
 	"net/http"
+	"os"
 )
+
+type Event struct {
+	AddEvent string
+	// Date string
+}
+
+type Data struct{
+	Events []Event
+}
 
 func Homehandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
@@ -21,4 +32,57 @@ func Homehandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func NewEventhandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+	http.ServeFile(w, r, "./web/templates/NewEvent.html")
+}
+
+func AddEventHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+	// fmt.Println("hi")
+	var events []Event
+	file, err := os.ReadFile("./database/events.json")
+	if err != nil {
+		http.Error(w, "hello", http.StatusInternalServerError)
+		return
+	}
+	json.Unmarshal(file, &events)
+	// if err != nil {
+	// 	http.Error(w, "hi", http.StatusInternalServerError)
+	// 	return
+	// }
+	newEvent := Event{
+		r.FormValue("AddEvent"),
+	}
+
+	events = append(events, newEvent)
+
+	jsonData, err := json.Marshal(events)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = os.WriteFile("./database/events.json", jsonData, 0o666)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	t, err := template.ParseFiles("./web/templates/NewEvent.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	data:=Data{Events: events}
+	err = t.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
 }
--- main.go
+++ main.go
@@ -7,7 +7,18 @@ import (
 
 func main() {
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/":
+			handler.Homehandler(w, r)
+		case "/NewEvent":
+			handler.NewEventhandler(w, r)
+		case "/AddEvent":
+			handler.AddEventHandler(w, r)
+		default:
+			http.Error(w, "404 Not Found", http.StatusNotFound)
+		}
+	})
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static/"))))
-	http.HandleFunc("/", handler.Homehandler)
 	http.ListenAndServe(":8080", nil)
 }
